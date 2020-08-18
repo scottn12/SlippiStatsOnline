9a164ed0e5e22ac55cab54e0ee18e30c154d1c2f
Coext <scott.norton12@gmail.com>
2020-08-18 19:11:52 -0400

    remove log color

--- backend/config/logger-config.js
+++ backend/config/logger-config.js
@@ -2,17 +2,15 @@ const winston = require('winston');
 const expressWinston = require('express-winston');
 
 const logger = winston.createLogger({
-    level: 'info',
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.colorize(),
         winston.format.json(),
     ),
 
     transports: [
         new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
         new winston.transports.File({ filename: 'logs/combined.log' }),
-        new winston.transports.Console()
+        new winston.transports.Console(),
     ],
 });
 
