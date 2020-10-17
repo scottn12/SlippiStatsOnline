9be0da3c6fe9192aa159667f1c3ff5cfb4e8a02f
Scott Norton <scott.norton12@gmail.com>
2020-10-17 13:39:02 -0400

    add theme specific bg color for path in how to use

--- frontend/src/plugins/theme.js
+++ frontend/src/plugins/theme.js
@@ -11,7 +11,8 @@ export default {
       error: '#e33a0b',
       loadingOverlay: colors.grey.lighten1,
       background: colors.shades.white,
-      menuBackground: '#44A963'
+      menuBackground: '#44A963',
+      pathBackground: colors.grey.lighten2
     },
     dark: {
       primary: '#44A963',
@@ -19,7 +20,8 @@ export default {
       error: '#e33a0b',
       loadingOverlay: colors.grey.darken1,
       background: colors.grey.darken3,
-      menuBackground: colors.grey.darken4
+      menuBackground: colors.grey.darken4,
+      pathBackground: colors.grey.darken1
     },
   },
 }
